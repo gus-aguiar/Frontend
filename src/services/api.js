ddbc6a159be65ee74a9b269cff2aadf6718cbda0
Leonardo Anzulin <leoanzulin@gmail.com>
2023-01-11 17:39:20 -0300

    req 1 completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = "https://api.mercadolibre.com/sites/MLB/categories";
+  return fetch(url).then((response) => response.json()).then((data) => data)
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  return fetch(url).then((response) => response.json()).then((data) => data)
 }
 
 export async function getProductById() {
