329a81d1b03270c7967c9e0636cd4f1aaf8d5743
Leonardo Anzulin <leoanzulin@gmail.com>
2023-01-11 17:43:22 -0300

    Req 1 finaliza com lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
-  const url = "https://api.mercadolibre.com/sites/MLB/categories";
-  return fetch(url).then((response) => response.json()).then((data) => data)
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return fetch(url).then((response) => response.json()).then((data) => data);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  return fetch(url).then((response) => response.json()).then((data) => data)
+  return fetch(url).then((response) => response.json()).then((data) => data);
 }
 
 export async function getProductById() {
