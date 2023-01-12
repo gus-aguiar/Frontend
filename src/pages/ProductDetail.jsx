2761729d32ca790cebfdd2e61c7fe2110fd83723
Leonardo Anzulin <leoanzulin@gmail.com>
2023-01-12 18:41:20 -0300

    Req 7 criando o card do produto no ProductDetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ProductDetail extends React.Component {
   render() {
+    const { product } = this.props;
     return (
-      <div />
+      <div key={ product.id } data-testid="product" type="button">
+        <p>{product.title}</p>
+        <img
+          src={ product.thumbnail }
+          alt={ product.title }
+        />
+        <p>{ product.price }</p>
+      </div>
 
     );
   }
 }
+
+ProductDetail.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string,
+    price: PropTypes.number,
+    thumbnail: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
 
 export default ProductDetail;
