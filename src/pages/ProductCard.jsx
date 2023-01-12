50d4dcc70b375272c66281e92037bf3e73ba536d
Leonardo Anzulin <leoanzulin@gmail.com>
2023-01-12 19:25:17 -0300

    Adicionando link ao ProductCart e tirando o button

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,18 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends React.Component {
   render() {
     const { product } = this.props;
     return (
-      <button key={ product.id } data-testid="product" type="button">
+      <div
+        key={ product.id }
+        data-testid="product"
+        type="button"
+      >
         <p>{product.title}</p>
         <img
           src={ product.thumbnail }
           alt={ product.title }
         />
         <p>{ product.price }</p>
-      </button>
+        <Link to="/cart">detalhes</Link>
+      </div>
 
     );
   }
